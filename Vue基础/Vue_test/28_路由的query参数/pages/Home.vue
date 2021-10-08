0a24b6eca9981806456127bd596f4ac3e2734803
yize <yize@2980.com>
2021-10-08 20:50:28 +0800

    路由的query参数

---
+++ Vue基础/Vue_test/28_路由的query参数/pages/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <h2>Home组件内容</h2>
+      <div>
+        <ul class="nav nav-tabs">
+          <li>
+            <router-link class="list-group-item " active-class="acive" to="/home/news">News</router-link>
+          </li>
+          <li>
+            <router-link class="list-group-item " active-class="acive" to="/home/message">Message</router-link>
+          </li>
+        </ul>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  beforeDestroy() {
+    console.log("Home组件即将被销毁了");
+  },
+  mounted() {
+    console.log("Home组件挂载完毕");
+    console.log(this);
+  },
+};
+</script>
