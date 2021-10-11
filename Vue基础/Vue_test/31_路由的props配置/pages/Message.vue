71cd280e8b8adb2a3013ac3bfab17838cd4882ab
yize <yize@2980.com>
2021-10-11 20:54:32 +0800

    路由的props配置

---
+++ Vue基础/Vue_test/31_路由的props配置/pages/Message.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div>
+      <ul>
+        <li v-for="m in messageList" :key="m.id">
+          <!-- 跳转路由并携带params参数，to的字符串写法 -->
+          <!-- <router-link :to="`/home/message/detail/${m.id}/${m.title}`">{{ m.title }}</router-link> -->
+          <!-- 跳转路由并携带params参数，to的对象写法 -->
+          <router-link
+            :to="{
+              name: 'detail',//不允许写path必须写name
+              params: {
+                id: m.id,
+                title: m.title,
+              },
+            }"
+            >{{ m.title }}</router-link
+          >
+        </li>
+      </ul>
+      <hr />
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  data() {
+    return {
+      messageList: [
+        { id: "001", title: "消息001" },
+        { id: "002", title: "消息002" },
+        { id: "003", title: "消息003" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
