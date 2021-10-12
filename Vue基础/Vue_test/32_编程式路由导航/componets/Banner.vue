5ac7d2200eaf9da91676a42edc2f6e9740869410
yize <yize@2980.com>
2021-10-12 20:36:57 +0800

    编程式路由导航

---
+++ Vue基础/Vue_test/32_编程式路由导航/componets/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-xs-offset-2 col-xs-8">
+    <div class="page-header">
+      <h2>Vue Router Demo</h2>
+      <button @click="back">后退</button>
+      <button @click="forward">前进</button>
+      <button @click="go">GO</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    back() {
+      this.$router.back();
+    },
+    forward() {
+      this.$router.forward();
+    },
+    go() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style></style>
