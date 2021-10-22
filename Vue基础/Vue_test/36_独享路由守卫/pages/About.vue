de8f058bdd803065f2e8e9dbee34c9b1840102c1
yize <yize@2980.com>
2021-10-22 20:35:04 +0800

    独享路由守卫

---
+++ Vue基础/Vue_test/36_独享路由守卫/pages/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>我是About</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  beforeDestroy() {
+    console.log("About组件即将被销毁了");
+  },
+  mounted() {
+    console.log("About组件挂在完毕");
+    console.log(this);
+  },
+};
+</script>
