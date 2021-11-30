e5fb4e46dcdc8008298a243f5fa81dfbd726981e
yize <yize@2980.com>
2021-11-30 20:59:34 +0800

    watch监视ref数据的说明

--- Vue基础/Vue3基础/Vue3快速上手.md
+++ Vue基础/Vue3基础/Vue3快速上手.md
@@ -321,3 +321,24 @@ setup(){
     );
 ```
 
+### 3.watchEffect
+
+- watch的套路是：既要指明监视属性，也要指明监视的回调
+
+- watchEffect的套路是：不用指明监视哪个属性，监视的回调中用到哪个属性，那就监视哪个属性
+
+- watchEffect有点像computed:
+
+  - 但computed注重的计算出来的值（回调函数的返回值），所以必须要写返回值
+  - 而wtachEffect更专注的是过程（回调函数的函数体），所以不用写返回值
+
+  ```
+  //wtachEffect所指定的回调中用到的数据只要发生变化，则直接重新执行回调
+  wtachEffect(()=>{
+  const x1 = sum.value
+  const x2 = person.value
+  console.log('wtachEffect配置的回调执行了')
+  })
+  ```
+
+  
---
+++ Vue基础/Vue3基础/vue3_test/09_watch监视ref的数据说明/components/demo.vue
@@ -0,0 +1,60 @@
+<template>
+  <h2>求和{{ sum }}</h2>
+  <button @click="sum++">加一</button>
+  <br />
+  <h2>当前的信息为：{{ msg }}</h2>
+  <button @click="msg += '!'">修改</button>
+  <hr />
+  <h2>姓名：{{ person.name }}</h2>
+  <h2>年龄：{{ person.age }}</h2>
+  <h2>薪资：{{ person.job.job1.s }}k</h2>
+  <button @click="person.name += '~'">修改姓名</button>
+  <button @click="person.age++">修改年龄</button>
+  <button @click="person.job.job1.s++">涨薪</button>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+export default {
+  name: "Demo",
+
+  setup() {
+    //数据
+    let sum = ref(0);
+    let msg = ref("你好");
+    let person = ref({
+      name: "张三",
+      age: 18,
+      job: {
+        job1: {
+          s: 20,
+        },
+      },
+    });
+
+    console.log(sum);
+    console.log(msg);
+    console.log(person);
+    
+    watch(sum,(n,o)=>{
+      console.log('sum的值变化了',n,o);
+    })
+
+    /* watch(person.value,(n,o)=>{
+      console.log('person的值变化了',n,o);
+    }) */
+
+    watch(person,(n,o)=>{
+      console.log('person的值变化了',n,o);
+    },{deep:true})
+    
+    return {
+      sum,
+      msg,
+      person,
+    };
+  },
+};
+</script>
+
+<style></style>
