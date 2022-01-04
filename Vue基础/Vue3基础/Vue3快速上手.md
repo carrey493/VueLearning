afd09a45999eebdbf5e0c4d12001d09c2c3e204d
yize <yize@2980.com>
2022-01-04 20:15:06 +0800

    readonly与shallowReadonly

--- Vue基础/Vue3基础/Vue3快速上手.md
+++ Vue基础/Vue3基础/Vue3快速上手.md
@@ -383,3 +383,10 @@ setup(){
 - 什么时候用？
   - 如果有一个对象数据，结构比较深，但变化时只是外层属性变化===》shallowReactive
   - 如果有一个对象数据，后续功能不会修改该对象中的属性，而是生成新的对象来替换===》shallowRef
+
+## 2.readonly与shallowReadonly
+
+- readonly:让一个响应式数据变为只读的（深只读）
+- shallowReadonly:让一个响应式数据变为只读的（浅只读）
+- 应用场景：不希望数据被修改时
+
---
+++ Vue基础/Vue3基础/vue3_test/14_readonly与shallowrReadonly/components/demo.vue
@@ -0,0 +1,41 @@
+<template>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h2>薪资：{{ job.job1.s }}k</h2>
+  <button @click="name += '~'">修改姓名</button>
+  <button @click="age++">修改年龄</button>
+  <button @click="job.job1.s++">涨薪</button>
+  <h2>当前求和：{{sum}}</h2>
+  <button @click="sum++">sum++</button>
+</template>
+
+<script>
+import { reactive, toRefs, ref,readonly,shallowReadonly } from "vue";
+export default {
+  name: "Demo",
+
+  setup() {
+    //数据
+    let person = reactive({
+      name: "张三",
+      age: 18,
+      job: {
+        job1: {
+          s: 20,
+        },
+      },
+    });
+    console.log(person);
+    // person = readonly(person)
+    person = shallowReadonly(person)
+    let sum = ref(0)
+    sum = readonly(sum)
+    return {
+      sum,
+      ...toRefs(person),
+    };
+  },
+};
+</script>
+
+<style></style>
