79c3c860eeb0bccc5c3d396a555a31e27e171002
Atrox493 <2577632081@qq.com>
2022-01-30 00:00:19 +0800

    响应式数据的判断

--- Vue基础/Vue3基础/Vue3快速上手.md
+++ Vue基础/Vue3基础/Vue3快速上手.md
@@ -467,5 +467,10 @@ setup() {
       ......
   }
   ```
+## 6.响应式数据的判断
 
-  
+- isRef:检查一个值是否是一个ref对象
+- isReactive:检查一个对象是否由reactive创建的响应式代理
+- isReadonly:检查一个对象是否由readonly创建的只读代理
+- isProxy:检查一个对象是否由reactive或者readonly方法创建的代理
+
---
+++ Vue基础/Vue3基础/vue3_test/18_响应式数据的判断/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <h3>我是APP组件</h3>
+  </div>
+</template>
+
+<script>
+import {
+  reactive,
+  toRefs,
+  ref,
+  readonly,
+  isReactive,
+  isReadonly,
+  isRef,
+  isProxy,
+} from "vue";
+export default {
+  name: "App",
+
+  setup() {
+    let car = reactive({
+      name: "BC",
+      price: "40",
+    });
+    let sum = ref(0);
+    let car2 = readonly(car);
+
+    console.log(isRef(sum));
+    console.log(isReactive(car));
+    console.log(isReadonly(car2));
+    console.log(isProxy(car));
+    console.log(isProxy(car2));
+
+    return { ...toRefs(car), sum, car2 };
+  },
+};
+</script>
+
+<style>
+.app {
+  background-color: skyblue;
+  padding: 10px;
+}
+</style>
